60d34a6797f2c8fce11b908735129c5a45ae4a4d
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2024-09-04 17:52:43 +0900

    add download script

---
+++ cached_download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#set -x
+
+DOWNLOAD_URL=$1
+DOWNLOAD_LOC=$2
+CACHE_SERVER_LOC="http://localhost:5000/download"
+
+curl --silent --show-error --fail -X POST $CACHE_SERVER_LOC -H 'Content-Type: application/x-www-form-urlencoded' -d "file_name=$DOWNLOAD_URL" --output $DOWNLOAD_LOC
+
+status=$?
+
+if [ $status -ne 0 ]; then
+  echo "Failed cache retrieval. Try downloading again from the original source."
+  curl $DOWNLOAD_URL --output $DOWNLOAD_LOC
+fi
+
+
