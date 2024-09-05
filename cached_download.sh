fd914dcf3cd5e84ac1afbbbb2f728f1ccbd55549
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2024-09-05 16:48:18 +0900

    update script

--- cached_download.sh
+++ cached_download.sh
@@ -4,7 +4,7 @@
 
 DOWNLOAD_URL=$1
 DOWNLOAD_LOC=$2
-CACHE_SERVER_LOC="http://localhost:5000/download"
+CACHE_SERVER_LOC=${CACHE_SERVER_LOC:-"http://localhost:7575/download"}
 
 curl --silent --show-error --fail -X POST $CACHE_SERVER_LOC -H 'Content-Type: application/x-www-form-urlencoded' -d "file_name=$DOWNLOAD_URL" --output $DOWNLOAD_LOC
 
@@ -12,7 +12,7 @@ status=$?
 
 if [ $status -ne 0 ]; then
   echo "Failed cache retrieval. Try downloading again from the original source."
-  curl --silent --show-error --fail $DOWNLOAD_URL --output $DOWNLOAD_LOC
+  curl --silent --show-error --fail -L $DOWNLOAD_URL --output $DOWNLOAD_LOC
 fi
 
 
