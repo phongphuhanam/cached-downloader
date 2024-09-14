4918a6ba5c475785cc9f5c8c0346c2768698f356
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2024-09-14 13:25:27 +0900

    try to add option to pipe command

--- cached_download.sh
+++ cached_download.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-#set -x
+set -x
 
 DOWNLOAD_URL=$1
 DOWNLOAD_LOC=$2
+IS_PIPE=${IS_PIPE:-"none"}
 CACHE_SERVER_LOC=${CACHE_SERVER_LOC:-"http://localhost:7575/download"}
 
-curl --silent --show-error --fail -X POST $CACHE_SERVER_LOC -H 'Content-Type: application/x-www-form-urlencoded' -d "file_name=$DOWNLOAD_URL" --output $DOWNLOAD_LOC
+DOWNLOAD_COMMAND="-H 'Content-Type: application/x-www-form-urlencoded'"
+DOWNLOAD_COMMAND+=" -d \"file_name=$DOWNLOAD_URL\""
+
+download() {
+  if ( "$IS_PIPE" == "pipe" ); then
+    curl --silent --show-error --fail -L $DOWNLOAD_COMMAND | $DOWNLOAD_LOC
+  else:
+    curl --silent --show-error --fail -L $DOWNLOAD_COMMAND $DOWNLOAD_LOC
+  fi
+}
+
+download $IS_PIPE $DOWNLOAD_COMMAND $DOWNLOAD_LOC 
+#curl --silent --show-error --fail -X POST $CACHE_SERVER_LOC -H 'Content-Type: application/x-www-form-urlencoded' -d "file_name=$DOWNLOAD_URL" $DOWNLOAD_LOC
 
 status=$?
 
 if [ $status -ne 0 ]; then
   echo "Failed cache retrieval. Try downloading again from the original source."
-  curl --silent --show-error --fail -L $DOWNLOAD_URL --output $DOWNLOAD_LOC
+  curl --silent --show-error --fail -L $DOWNLOAD_URL $DOWNLOAD_LOC
 fi
 
 
